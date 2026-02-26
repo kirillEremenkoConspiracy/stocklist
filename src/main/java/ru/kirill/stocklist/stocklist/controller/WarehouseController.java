61f080a24a4498edb5f5001b8396a8ea2be7a1dc
KirillEremenko <KirillEremenko10@mail.ru>
2026-02-26 17:46:25 +0300

    v0.0.1 Добавлен POST. Создание склада

--- src/main/java/ru/kirill/stocklist/stocklist/controller/WarehouseController.java
+++ src/main/java/ru/kirill/stocklist/stocklist/controller/WarehouseController.java
@@ -3,6 +3,9 @@ package ru.kirill.stocklist.stocklist.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.kirill.stocklist.stocklist.domain.Warehouse;
 import ru.kirill.stocklist.stocklist.repository.WarehouseRepository;
 
 @Controller
@@ -18,5 +21,20 @@ public class WarehouseController {
     public String list(Model model) {
         model.addAttribute("warehouses", warehouseRepository.findAll());
         return "warehouses";
+    }
+
+    //GET
+    @GetMapping("/warehouses/new")
+    public String newWarehouse(Model model){
+        model.addAttribute("warehouseForm", new WarehouseForm());
+        return "warehouse-form";
+    }
+
+    //создание склада POST(ловит POST запросы)
+    @PostMapping("/warehouses")
+    public String createWarehouse(@ModelAttribute("warehouseForm") WarehouseForm form){
+        Warehouse w = new Warehouse(form.getName(), form.getAddress());
+        warehouseRepository.save(w);
+        return "redirect:/warehouses";
     }
 }
